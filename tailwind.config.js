1e3b76e8fbc515eac1e511e59745daf5b0a9a03f
haidar miqdad <haidarmiqdad@Muhamads-MacBook-Air.local>
2023-09-14 16:34:57 +0700

    slacinng

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container:{
+      center: true,
+      // padding: '16px'
+    },
+    extend: {
+      colors: {
+        bg: '#25C7D1',
+       bg2: '#a5f3fc',
+      },
+    },
+  },
+  plugins: [],
+}
+
